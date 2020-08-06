aa05e98a0a659ffd5144e417b8e2fbca377f9188
michaelcase1 <65096782+michaelcase1@users.noreply.github.com>
2020-08-06 15:15:32 -0700

    init

---
+++ week-3/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+for (var i = 0; i < 10; i++){
+    var newH1 = document.createElement("h1")
+    newH1.textContent = "HELLO WORLD"
+    newH1.style.color = "red"
+    
+    document.body.append(newH1)
+  }
+  
+  var  constnames = ["steve","larry","joe","shirley","steph","nate","rick","emily"]
+  for (var j = 0; j < constnames.length; j++);
+  console.log(j)
