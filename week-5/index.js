0e589fac6f7f50666cf285374d914c907a6723d3
michael puglisi <mickeyispicky002@gmail.com>
2020-10-06 21:24:56 -0700

    init

---
+++ week-5/index.js
@@ -0,0 +1,29 @@
+const form = document.form
+
+var button = document.getElementById("button")
+var state = document.getElementsByTagName("State")
+console.log(state)
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+console.log(form.gender.value)
+    
+ console.log(form.entertainment.value)
+})
+
+const checkedInputs = []
+    
+    for(let i = 0; i < form.dietaryRestrictions.length; i++){
+        if(form.dietaryRestrictions[i].checked){
+            checkedInputs.push(form.dietaryRestrictions[i].value)
+        }
+    }
+    console.log(checkedInputs)
+    function test(){
+        alert (location)
+    }
+
+
+ 
+button.addEventListener("click", test)
