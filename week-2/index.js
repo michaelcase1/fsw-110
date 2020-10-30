66d6058291afa4e28d799c633e3e5bf822746e9f
michael puglisi <mickeyispicky002@gmail.com>
2020-10-30 12:05:39 -0700

    appending init

--- week-2/index.js
+++ week-2/index.js
@@ -1,25 +1,43 @@
 
-
-
-
-
-
+//CREATE
+//ADD TEXT
+//GET ELEMENT
+//APPEND
 var newLi = document.createElement("li")
 newLi.textContent = "two"
 var myList = document.getElementById("my-list")
 myList.append(newLi)
 
-var newLi = document.createElement("li")
-newLi.textContent = "three"
-var myList = document.getElementById("my-list")
-myList.append(newLi)
+var newLi2 = document.createElement("li")
+newLi2.textContent = "three"
+myList.append(newLi2)
 
-var newLi = document.createElement("li")
-newLi.textContent = "four"
-var myList = document.getElementById("my-list")
-myList.append(newLi)
+var newLi3 = document.createElement("li")
+newLi3.textContent = "four"
+myList.append(newLi3)
 
+var newH1 = document.createElement("h1")
+newH1.textContent = "this is my h1"
+document.body.append(newH1)
 
 var newP = document.createElement("p")
-newP.textContent = "All this was created with javascript"
+newP.textContent = "this is my paragraph. all of this was created with Javascript"
 document.body.append(newP)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
